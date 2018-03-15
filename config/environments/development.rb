ce1f8cb80ef3f2166901f8d6116ea827fbcb46c2
Duongtnhat <duongtnhat@gmail.com>
2018-03-15 17:33:57 +0700

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
-  # config.active_support.deprecation = :log
-
-  # Raise an error on page load if there are pending migrations.
-  # config.active_record.migration_error = :page_load
+  config.active_support.deprecation = :log
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
