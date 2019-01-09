14e16b2883d6c9c10ab52deaf8331ba19767aafc
DuongTuanNhat <duong.tuan.nhat@framgia.com>
2019-01-09 16:46:06 +0700

    Maintain after long time

--- app/controllers/btl_controller.rb
+++ app/controllers/btl_controller.rb
@@ -1,75 +1,85 @@
 class BtlController < ApplicationController
 
-  #router("/")
-  #router("/:page")
+  include HTTParty
+  include HTTParty::DryIce
+  cache Rails.cache
+
   def index
     page = request.parameters[:page] || 'page-1'
     url = 'http://m.blogtruyen.com/' + page
-    response = HTTParty.get url
-    html_response = response.body
+    html_response = http_get(url)
     @items = extract_item html_response
     page.sub!(/page-/, '')
     @page = page.to_i
+    expires_in 6.hours, public: true
     render 'btl/index'
   end
 
-  #router("/:id/:name")
   def story
     url = 'http://blogtruyen.com/ajax/Chapter/PartialLoadListChapter?mangaId='+request.parameters[:id]
-    response = HTTParty.get url
-    @items = extract_chapter response.body
+    @items = extract_chapter http_get(url)
+    expires_in 6.hours, public: true
     render 'btl/story'
   end
 
-  #router("/:cid/:name")
   def chapter
     id = request.parameters[:id]
     name = request.parameters[:name]
     url = 'http://m.blogtruyen.com/%s/%s' % [id, name]
-    response = HTTParty.get url
-    html = Nokogiri::HTML(response)
+    html = Nokogiri::HTML(http_get(url))
     @post = html.xpath('//div[@class="content"]').first
     list = html.xpath('//select[@class="form-control"]').first
     @list = list
     @selected = list.xpath('.//option[@selected="selected"]').first
     @pre = @selected.next.nil? ? nil : @selected.next.attr("value")
     @next = @selected.previous.nil? ? nil : @selected.previous.attr("value")
+    expires_in 6.hours, public: true
     render 'btl/chapter'
   end
 
-  #route("/timkiem?keyword=:keyword&p=:page")
   def search
     url = 'http://m.blogtruyen.com/timkiem?keyword=' + request.parameters[:keyword]
     url += '&p=' + request.parameters[:p] unless request.parameters[:p].nil?
-    response = HTTParty.get url
+    response = http_get(url)
     html = Nokogiri::HTML(response)
     @result = html.xpath('//article').first
     render 'btl/search'
   end
 
+  private
+
   def extract_item(html_response)
     html = Nokogiri::HTML(html_response)
     ul = html.xpath('//ul[contains(@class, "list-unstyled") and contains(@class, "list-manga")]')
     list = []
-    ul.xpath('.//li').each {|e|
+    ul.xpath('.//li[contains(@class, "item")]').each do |e|
       link = e.xpath('.//a/@href').first.value
       title = e.xpath('.//span').first.content
       img = e.xpath('.//img/@src').first.value
-      list.push ({'link' => link, 'title' => title, 'img' => img})
-    }
-    return list
+      list << { link: link, title: title, img: img }
+    end
+    list
   end
 
   def extract_chapter(html_response)
     html = Nokogiri::HTML(html_response)
     div = html.xpath('//div[contains(@class, "list-wrap")]')
     list = []
-    div.xpath('.//p').each {|e|
+    div.xpath('.//p').each do |e|
       link = e.xpath('.//a/@href').first.value
       title = e.xpath('.//a').first.content
-      published_date = e.xpath('.//span[contains(@class, "publishedDate")]').first.content
-      list.push ({'link' => link, 'title' => title, 'date' => published_date})
-    }
-    return list
+      list << { link: link, title: title }
+    end
+    list
+  end
+
+  def http_get(url)
+    Rails.cache.fetch(url) do
+      logger.info "Load " << url
+      response = HTTParty.get url
+      break unless response.code == 200
+      logger.info "Complete load " << url
+      response.body
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'btl#index'
-  get '/:id/:name', to: 'btl#story' , :constraints => { :id => /\d+/ }
-  get '/:id/:name', to: 'btl#chapter' , :constraints => { :id => /c\d+/ }
-  get '/:page', to: 'btl#index', :constraints => { :page => /page-\d+/ }
-  get '/timkiem', to: 'btl#search'
+  get '/:id/:name', to: 'btl#story', constraints: { id: /\d+/ }, as: :story
+  get '/:id/:name', to: 'btl#chapter', constraints: { id: /c\d+/ }, as: :chapter
+  get '/:page', to: 'btl#index', constraints: { page: /page-\d+/ }, as: :page
+  get '/timkiem', to: 'btl#search', as: :search
 end
